b68ec34397936ca0ccef90e3c9ad1cdb199f9dfc
uhuguette4 <u.huguette@alustudent.com>
2026-02-06 23:10:11 +0000

    updated 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,21 @@
 #!/usr/bin/node
+// Script that reads and prints the content of a file
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.log('Usage: node 0-readme.js <file_path>');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
+    return;
   }
+  console.log(data);
 });
+
 
